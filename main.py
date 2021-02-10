6830413d7f25a6d97b4d106043561515988b3028
Deni <azureswastika@gmail.com>
2021-02-10 14:42:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ FONT = "src/Oswald-Regular.ttf"
 FFORMAT = "png"
 CONVERT = "RGB"
 
+static = Path(DATA_DIR)
+output = Path(OUTPUT_DIR)
+output.mkdir(exist_ok=True)
+header = ImageFont.truetype(FONT, 55)
+paragraph = ImageFont.truetype(FONT, 15)
+white = "white"
+black = (0, 0, 0, 0)
+blue = (40, 101, 176, 0)
+
 
 class Base:
     def __init__(self, fp: str) -> None:
@@ -38,27 +47,17 @@ class Geneartor:
     f_backend = ImageDescriptor()
     f_main = ImageDescriptor()
 
-    static = Path(DATA_DIR)
-    output = Path(OUTPUT_DIR)
-    output.mkdir(exist_ok=True)
-
     backend = Base(SRC_BACKEND_IMG)
     main = Base(SRC_MAIN_IMG)
     frontend = Base(SRC_FRONTEND_IMG)
 
-    header = ImageFont.truetype(FONT, 55)
-    paragraph = ImageFont.truetype(FONT, 15)
-    white = "white"
-    black = (0, 0, 0, 0)
-    blue = (40, 101, 176, 0)
-
     def __init__(self, obj) -> None:
         self.name = obj.name
-        self.output.joinpath(self.name).mkdir(exist_ok=True)
-        self.output.joinpath(self.name, "frontend").mkdir(exist_ok=True)
+        output.joinpath(self.name).mkdir(exist_ok=True)
+        output.joinpath(self.name, "frontend").mkdir(exist_ok=True)
         self.title = self.process_text(obj.backend.title)
-        self.f_backend = Image.open(self.static.joinpath(obj.backend.backend))
-        self.f_main = Image.open(self.static.joinpath(obj.backend.main))
+        self.f_backend = Image.open(static.joinpath(obj.backend.backend))
+        self.f_main = Image.open(static.joinpath(obj.backend.main))
         self.cards = obj.cards
 
     def start(self) -> None:
@@ -73,9 +72,9 @@ class Geneartor:
         )
         self.backend.image.paste(self.f_backend, box, self.f_backend.convert("RGBA"))
         draw = ImageDraw.Draw(self.backend.image)
-        draw = self.add_title(draw, self.title, self.blue, 1150)
+        draw = self.add_title(draw, self.title, blue, 1150)
         self.backend.image.convert(CONVERT).save(
-            self.output.joinpath(f"{self.name}/backend.{FFORMAT}")
+            output.joinpath(f"{self.name}/backend.{FFORMAT}")
         )
         self.backend.reset()
 
@@ -86,19 +85,19 @@ class Geneartor:
         )
         self.main.image.paste(self.f_main, box, self.f_main.convert("RGBA"))
         draw = ImageDraw.Draw(self.main.image)
-        w, h = draw.textsize("0", self.header)
+        w, h = draw.textsize("0", header)
         box = (112 - w // 2, 112 - h // 1.5)
         draw.text(
-            box, "0", font=self.header, fill=self.white,
+            box, "0", font=header, fill=white,
         )
         self.main.image.convert(CONVERT).save(
-            self.output.joinpath(f"{self.name}/main.{FFORMAT}")
+            output.joinpath(f"{self.name}/main.{FFORMAT}")
         )
         self.main.reset()
 
     def create_frontend(self):
         for el in self.cards:
-            image = Image.open(self.static.joinpath(el.image))
+            image = Image.open(static.joinpath(el.image))
             if image.width > 710 or image.height > 590:
                 image.thumbnail((710, 590), Image.ANTIALIAS)
             box = (
@@ -107,16 +106,16 @@ class Geneartor:
             )
             self.frontend.image.paste(image, box, image.convert("RGBA"))
             draw = ImageDraw.Draw(self.frontend.image)
-            w, h = draw.textsize(str(el.num), self.header)
+            w, h = draw.textsize(str(el.num), header)
             box = (112 - w // 2, 112 - h // 1.5)
-            draw.text(box, str(el.num), font=self.header, fill=self.black)
+            draw.text(box, str(el.num), font=header, fill=black)
             title = self.process_text(el.title)
-            draw = self.add_title(draw, title, self.black)
+            draw = self.add_title(draw, title, black)
             draw.multiline_text(
-                (90, 960), fill(el.block1, 58), font=self.paragraph, fill=self.black,
+                (90, 960), fill(el.block1, 58), font=paragraph, fill=black,
             )
             draw.multiline_text(
-                (490, 960), fill(el.block2, 58), font=self.paragraph, fill=self.black,
+                (490, 960), fill(el.block2, 58), font=paragraph, fill=black,
             )
             self.frontend.image.convert(CONVERT).save(
                 f"{OUTPUT_DIR}/{self.name}/frontend/{el.num}.{FFORMAT}"
@@ -132,10 +131,10 @@ class Geneartor:
             box = self.process_heigth(w, h, heigth)
             draw.multiline_text(box, title, font=subheader, fill=fill, align="center")
             return draw
-        w, h = draw.textsize(title, self.header)
+        w, h = draw.textsize(title, header)
         box = self.process_heigth(w, h, heigth)
         draw.multiline_text(
-            box, title, font=self.header, fill=fill, align="center",
+            box, title, font=header, fill=fill, align="center",
         )
         return draw
 
