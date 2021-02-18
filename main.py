b5e997ce7e92d469213dac78fb06f627956cc4e9
Deni <azureswastika@gmail.com>
2021-02-18 15:08:27 +0300

    Fix pdf generator

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from shutil import rmtree
 from textwrap import fill, wrap
 
 from PIL import Image, ImageDraw, ImageFont
@@ -52,21 +53,21 @@ class Geneartor:
     main = Base(SRC_MAIN_IMG)
     frontend = Base(SRC_FRONTEND_IMG)
 
-    output_files = list()
-
     def __init__(self, obj) -> None:
         self.name = obj.name
-        output.joinpath(self.name).mkdir(exist_ok=True)
-        output.joinpath(self.name, "frontend").mkdir(exist_ok=True)
         self.title = self.process_text(obj.backend.title)
         self.f_backend = Image.open(static.joinpath(obj.backend.backend))
         self.f_main = Image.open(static.joinpath(obj.backend.main))
         self.cards = obj.cards
+        rmtree(output.joinpath(self.name), ignore_errors=True)
+        output.joinpath(self.name).mkdir(exist_ok=True)
+        output.joinpath(self.name, "frontend").mkdir(exist_ok=True)
+        self.output_files = list()
 
     def start(self) -> None:
         self.create_backend()
-        self.create_frontend()
         self.create_main()
+        self.create_frontend()
         self.create_pdf()
 
     def create_backend(self) -> None:
@@ -93,7 +94,10 @@ class Geneartor:
         w, h = draw.textsize("0", header)
         box = (112 - w // 2, 112 - h // 1.5)
         draw.text(
-            box, "0", font=header, fill=white,
+            box,
+            "0",
+            font=header,
+            fill=white,
         )
         self.main.image.convert(CONVERT).save(
             output.joinpath(f"{self.name}/main.{FFORMAT}")
@@ -118,15 +122,23 @@ class Geneartor:
             title = self.process_text(el.title)
             draw = self.add_title(draw, title, black)
             draw.multiline_text(
-                (90, 960), fill(el.block1, 58), font=paragraph, fill=black,
+                (90, 960),
+                fill(el.block1, 58),
+                font=paragraph,
+                fill=black,
             )
             draw.multiline_text(
-                (490, 960), fill(el.block2, 58), font=paragraph, fill=black,
+                (490, 960),
+                fill(el.block2, 58),
+                font=paragraph,
+                fill=black,
             )
             self.frontend.image.convert(CONVERT).save(
                 f"{OUTPUT_DIR}/{self.name}/frontend/{el.num}.{FFORMAT}"
             )
-            self.output_files.append(f"{OUTPUT_DIR}/{self.name}/frontend/{el.num}.{FFORMAT}")
+            self.output_files.append(
+                f"{OUTPUT_DIR}/{self.name}/frontend/{el.num}.{FFORMAT}"
+            )
             self.frontend.reset()
 
     def add_title(self, draw, title, fill, heigth=None):
@@ -141,18 +153,28 @@ class Geneartor:
         w, h = draw.textsize(title, header)
         box = self.process_heigth(w, h, heigth)
         draw.multiline_text(
-            box, title, font=header, fill=fill, align="center",
+            box,
+            title,
+            font=header,
+            fill=fill,
+            align="center",
         )
         return draw
 
-    def create_pdf(self, step=3):
-        # FIXME
+    def create_pdf(self, step=4):
         pdf = canvas.Canvas(f"{OUTPUT_DIR}/{self.name}/pdf_file.pdf")
         pdf.setPageSize((2480, 3508))
-        for i in range(0, len(self.output_files), step):
-            images = self.output_files[i:i + step]
+        sizes = [(100, 2000), (1400, 2000), (100, 500), (1400, 500)]
+        while self.output_files:
+            images = self.output_files[:step]
+            for s, f in zip(sizes, images):
+                pdf.drawImage(f, *s)
+            pdf.showPage()
+            for s, f in zip(sizes, [self.backend_path for i in range(len(images))]):
+                pdf.drawImage(f, *s)
+            pdf.showPage()
+            self.output_files = self.output_files[step:]
         pdf.save()
-        self.output_files = list()
 
     def process_heigth(self, w, h, heigth):
         if heigth:
